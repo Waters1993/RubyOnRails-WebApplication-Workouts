df7163411de6a6b021874ce5ca48189601931bde
James Waters <jwaters1993@outlook.com>
2022-08-08 22:37:28 -0400

    Added login in user to Global template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # Adding a filter method
+    before_action :current_user
+
+    protected
+
+    def current_user
+        return unless session[:user_id]
+        @current_user = User.where(id: session[:user_id]).first    
+    end
+
+
 end
